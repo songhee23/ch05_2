683142170652118ca68df4d5ee59624bc8cd8b97
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-30 16:27:31 +0900

    필요한 파일 복사 및, 필요없는 파일 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import {Provider as ReduxProvider} from 'react-redux'
+import { useStore } from './store'
+
+import ReduxClock from './pages/ReduxClock'
+import UseReducerClock from './pages/UseReducerClock'
+
+
+
+export default function App() {
+  const store = useStore()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    
+    <ReduxProvider store={store}>
+      <main className="p-8">
+        <UseReducerClock />
+        <ReduxClock />
+      </main>
+    </ReduxProvider>
 
-export default App;
+  )
+}
