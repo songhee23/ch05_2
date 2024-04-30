76aa3e13bddbed718ce175ce2b132730d0d73ba9
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-30 16:53:44 +0900

    App.tsx 실습할 컴포넌트를 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@
 import {Provider as ReduxProvider} from 'react-redux'
 import { useStore } from './store'
 
-
+import ClockTest from './pages/ClockTest'
+import CounterTest from './pages/CounterTest'
+import RemoteUserTest from './pages/RemoteUserTest'
+import CardsTest from './pages/CardsTest'
 
 export default function App() {
   const store = useStore()
   return (
-    
     <ReduxProvider store={store}>
-        <div/>
+      <CardsTest />
+      <RemoteUserTest />
+      <CounterTest />
+      <ClockTest />
     </ReduxProvider>
 
   )
