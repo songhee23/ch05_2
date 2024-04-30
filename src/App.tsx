0c8d56f9a28eddcc84c771369eb3971a8b5d8c15
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-30 16:31:16 +0900

    npm start하면 화면에 아무것도 안보이지만 src/store쪽 리덕스 기능 정상 동작 확인

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@
 import {Provider as ReduxProvider} from 'react-redux'
 import { useStore } from './store'
 
-import ReduxClock from './pages/ReduxClock'
-import UseReducerClock from './pages/UseReducerClock'
-
 
 
 export default function App() {
@@ -13,8 +10,7 @@ export default function App() {
     
     <ReduxProvider store={store}>
       <main className="p-8">
-        <UseReducerClock />
-        <ReduxClock />
+
       </main>
     </ReduxProvider>
 
